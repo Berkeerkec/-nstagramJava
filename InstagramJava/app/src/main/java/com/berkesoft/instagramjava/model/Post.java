5102ecb686b30ec9a1de29664d9263fa6008e279
Berkeerkec <101473351+Berkeerkec@users.noreply.github.com>
2022-10-25 12:06:42 +0300

    Instagram Klonu
    Uygulama içerisine istediğiniz paylaşımları yapabileceğiniz bir sosyal paylaşım platformudur. Verilerin depolanması için Firebase kullanıldı.

---
+++ InstagramJava/app/src/main/java/com/berkesoft/instagramjava/model/Post.java
@@ -0,0 +1,18 @@
+package com.berkesoft.instagramjava.model;
+
+public class Post {
+
+    public String email;
+    public String downloadurl;
+    public String comment;
+
+    public Post(String email, String downloadurl, String comment){
+        this.email = email;
+        this.downloadurl = downloadurl;
+        this.comment = comment;
+    }
+
+
+
+
+}
